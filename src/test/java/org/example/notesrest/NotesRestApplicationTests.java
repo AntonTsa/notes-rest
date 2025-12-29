bb77d9f32c8516fc16947e53f8ab8aa7324c14a6
Anton Tsarenko <itantontsarenko@gmail.com>
2025-12-29 19:27:28 +0200

    fix: created test profile

--- src/test/java/org/example/notesrest/NotesRestApplicationTests.java
+++ src/test/java/org/example/notesrest/NotesRestApplicationTests.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = NotesRestApplication.class, webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
 class NotesRestApplicationTests {
 
     @Autowired
